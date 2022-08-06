a57af226bc4f1afba9586ff91ca75c80d9b3d369
luckvava <lucaskaiquefreitas15@gmail.com>
2022-08-05 21:44:19 -0300

    trabalho do curso
    trabalho do curso

---
+++ script.js
@@ -0,0 +1,12 @@
+function logar(){ 
+    var login = document.getElementById("login").value
+    var senha = document.getElementById("senha").value;
+    
+    if(login == "cavalo" && senha == "admin"){
+        alert("Login realizado com sucesso!");
+       location.href="index.html";
+    }else{
+        alert("Usuário ou senha incorretos!");
+    }
+
+}
